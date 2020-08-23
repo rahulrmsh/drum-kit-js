c0529a785d84a47e7e159eb7bdc89093ec1d1cc7
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-08-23 22:07:34 +0530

    code updated

--- js/dist/index.dev.js
+++ js/dist/index.dev.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var firstBtn = document.querySelector('#first');
 var secondBtn = document.querySelector('#second');
 var thirdBtn = document.querySelector('#third');
@@ -5,8 +7,8 @@ var fourthBtn = document.querySelector('#fourth');
 var fifthBtn = document.querySelector('#fifth');
 var sixthBtn = document.querySelector('#sixth');
 
-firstBtn.onclick = function() {
-    music.pause();
-    music = new Audio('sounds/Basic_Rock_Crash_135.mp3');
-    music.play();
+firstBtn.onclick = function () {
+  music.pause();
+  music = new Audio('sounds/Basic_Rock_Crash_135.mp3');
+  music.play();
 };
