cd8019ccddfdfdca3b172679e6683aa44f64c2a9
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-08-24 22:20:54 +0530

    audio synced

--- js/dist/index.dev.js
+++ js/dist/index.dev.js
@@ -29,6 +29,30 @@ for (var i = 0; i < document.querySelectorAll(".btn-danger").length; i++) {
         audioPlayer('sounds/Pounder_130.mp3');
         break;
 
+      case 'RatAtat':
+        audioPlayer('sounds/RatAtat_155.mp3');
+        break;
+
+      case 'First Beat':
+        audioPlayer('sounds/first_beat.mp3');
+        break;
+
+      case 'Tamba':
+        audioPlayer('sounds/Tamba_112.mp3');
+        break;
+
+      case 'Fill Crash':
+        audioPlayer('sounds/Basic_Rock_FillintoCra#19A6.mp3');
+        break;
+
+      case 'Boom Chuk':
+        audioPlayer('sounds/Boom_Chuk_130.mp3');
+        break;
+
+      case 'Basic Fill':
+        audioPlayer('sounds/Basic_Rock_FillintoCrash_135.mp3');
+        break;
+
       default:
         break;
     }
