b5c2655b9a85cb112a63a55c18f1790de59a758d
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-08-24 20:03:18 +0530

    sounds synced

--- js/dist/index.dev.js
+++ js/dist/index.dev.js
@@ -1,7 +1,41 @@
 "use strict";
 
-document.querySelector("button").addEventListener("click", handleClicked);
+for (var i = 0; i < document.querySelectorAll(".btn-danger").length; i++) {
+  document.querySelectorAll(".btn-danger")[i].addEventListener("click", function () {
+    var soundName = this.textContent;
 
-function handleClicked() {
-  alert("Clicked");
+    switch (soundName) {
+      case 'Basic Rock Crash':
+        audioPlayer('sounds/Basic_Rock_Crash_135.mp3');
+        break;
+
+      case 'Big Thwack':
+        audioPlayer('sounds/Big_Thwack_120.mp3');
+        break;
+
+      case 'Boom Chuk':
+        audioPlayer('sounds/Boom_Chuk_130.mp3');
+        break;
+
+      case 'Cymbal Groove':
+        audioPlayer('sounds/Cymbal_Groove.mp3');
+        break;
+
+      case 'Driving Force':
+        audioPlayer('sounds/Driving_Force_120.mp3');
+        break;
+
+      case 'Pounder':
+        audioPlayer('sounds/Pounder_130.mp3');
+        break;
+
+      default:
+        break;
+    }
+  });
+}
+
+function audioPlayer(url) {
+  var audio = new Audio(url);
+  audio.play();
 }
--- js/index.js
+++ js/index.js
@@ -1,5 +1,32 @@
-document.querySelector("button").addEventListener("click", handleClicked);
+for (var i = 0; i < document.querySelectorAll(".btn-danger").length; i++) {
+    document.querySelectorAll(".btn-danger")[i].addEventListener("click", function() {
+        var soundName = this.textContent;
+        switch (soundName) {
+            case 'Basic Rock Crash':
+                audioPlayer('sounds/Basic_Rock_Crash_135.mp3');
+                break;
+            case 'Big Thwack':
+                audioPlayer('sounds/Big_Thwack_120.mp3');
+                break;
+            case 'Boom Chuk':
+                audioPlayer('sounds/Boom_Chuk_130.mp3');
+                break;
+            case 'Cymbal Groove':
+                audioPlayer('sounds/Cymbal_Groove.mp3');
+                break;
+            case 'Driving Force':
+                audioPlayer('sounds/Driving_Force_120.mp3');
+                break;
+            case 'Pounder':
+                audioPlayer('sounds/Pounder_130.mp3');
+                break;
+            default:
+                break;
+        }
+    });
+}
 
-function handleClicked() {
-    alert("Clicked");
+function audioPlayer(url) {
+    var audio = new Audio(url);
+    audio.play();
 }
